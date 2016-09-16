3f4266388474d6dd87e7da3d28b7ce710de3e4f9
Christine Chang <christine.chang@openvpn-client.vpn.appdynamics.com>
2016-09-16 08:34:59 -0700

    Initial Commit

---
+++ MemeMe1Tests/MemeMe1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MemeMe1Tests.swift
+//  MemeMe1Tests
+//
+//  Created by Christine Chang on 9/16/16.
+//  Copyright © 2016 Christine Chang. All rights reserved.
+//
+
+import XCTest
+@testable import MemeMe1
+
+class MemeMe1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
